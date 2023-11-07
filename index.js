9a9f803534dc676ac420f7833d04009dc9ee86fd
Taggart <t.salcedo@hotmail.com>
2023-11-07 08:47:30 -0500

    updates on JS

---
+++ index.js
@@ -0,0 +1,22 @@
+//link js to html
+//done
+//create prompt for flavors
+const custOrder = prompt(`Please enter your flavor here`); 
+//create array to store inputs
+const custArr = custOrder.split(`,`);
+//create const for flavores
+
+const flaVanilla= `vanilla`;
+const flaStrawberry= `strawberry`;
+const flaCoffee= `coffee`;
+
+//create loop 
+let iceCream = {   
+
+}
+for (i=0;i<custArr.length;i++) {
+ const iceCream = custArr[i]
+ iceCream [flavor] = 1
+}
+console.log(flaCoffee);
+
